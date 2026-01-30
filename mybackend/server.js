11bd3472827efc8f83cf634b6ca8b6b5091dbc8b
raghav <raghavvenkat18@gmail.com>
2026-01-30 23:55:31 +0530

    third

--- mybackend/server.js
+++ mybackend/server.js
@@ -6,20 +6,12 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-const allowedOrigins = [
-    'http://localhost:5173',
-    process.env.FRONTEND_URL
-].filter(Boolean);
-
+// Middleware
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
